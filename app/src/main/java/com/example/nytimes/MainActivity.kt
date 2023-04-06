f0bc2e12dd06a9671138c870eec4e4254eafa625
Amrita <amritav.ca@gmail.com>
2023-04-06 14:44:53 -0700

    dismiss keyboard by pressing on screen

--- app/src/main/java/com/example/nytimes/MainActivity.kt
+++ app/src/main/java/com/example/nytimes/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -21,7 +23,9 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.nytimes.ui.theme.MyApplicationTheme
@@ -44,23 +48,30 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun UserInput(modifier: Modifier) {
     var name by remember { mutableStateOf("") }
+    val keyboardController = LocalSoftwareKeyboardController.current
 
-    Column(
-        modifier = modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Box(modifier = modifier.width(240.dp)) {
-            TextField(
-                value = name,
-                onValueChange = {name = it},
-                label = { Text("Enter your desired username:") },
-                modifier = modifier.fillMaxWidth()
-            )
+    Box(modifier =   modifier.fillMaxWidth().clickable(onClick = {
+        // Dismiss the keyboard when the user clicks outside of the TextField
+        keyboardController?.hide()
+        // maybe remove ripple effect?
+    })) {
+        Column(
+            modifier = modifier.fillMaxSize().padding(top = 20.dp),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(modifier = modifier.width(240.dp)) {
+                TextField(
+                    value = name,
+                    onValueChange = { name = it },
+                    label = { Text("Enter your desired username:") },
+                    modifier = modifier.fillMaxWidth()
+                )
+            }
         }
     }
 
