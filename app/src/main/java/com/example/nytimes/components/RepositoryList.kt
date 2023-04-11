fac53a744ee9f1bb4296caf69319047a6bf30467
Amrita <amritav.ca@gmail.com>
2023-04-11 13:11:17 -0700

    add web view instead

--- app/src/main/java/com/example/nytimes/components/RepositoryList.kt
+++ app/src/main/java/com/example/nytimes/components/RepositoryList.kt
@@ -1,7 +1,12 @@
 package com.example.nytimes.components
 
+import android.app.Activity
 import android.content.Context
+import android.content.Intent
 import android.net.Uri
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -12,10 +17,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import com.example.nytimes.R
 import com.example.nytimes.model.Repository
@@ -28,6 +38,7 @@ import com.example.nytimes.model.Repository
 @Composable
 fun RepositoryList(modifier: Modifier, repositories: List<Repository>) {
     val context = LocalContext.current
+    var url by remember { mutableStateOf("") }
     var index = 1
     LazyColumn {
         items(repositories) { repo ->
@@ -41,7 +52,8 @@ fun RepositoryList(modifier: Modifier, repositories: List<Repository>) {
                     text = "${index}. ${repo.fullName}",
                     modifier = modifier
                         .clickable {
-                            openCustomTab(context, Uri.parse(repo.url))
+                            url = repo.url
+                            // openCustomTab(context, Uri.parse(repo.url))
                         },
                     fontWeight = FontWeight.Bold
                 )
@@ -52,20 +64,32 @@ fun RepositoryList(modifier: Modifier, repositories: List<Repository>) {
             index++
         }
     }
+    AndroidView(modifier = modifier.padding(top = 20.dp), factory = { context ->
+        WebView(context).apply {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            loadUrl(url)
+        }
+    },
+        update = { view -> view.loadUrl(url) })
 }
+
 
 /**
  * Opens Custom Chrome tab if user clicks on one of the listed repositories.
  */
 private fun openCustomTab(context: Context, url: Uri) {
     val packageName = "com.android.chrome"
+    val activity = (context as? Activity)
     val customBuilder = CustomTabsIntent.Builder()
         .setShowTitle(true)
         .setInstantAppsEnabled(true) // enabling Instant Apps to open if available
         .setToolbarColor(ContextCompat.getColor(context, R.color.purple_40))
         .build()
 
-    // if chrome is installed, launch custom tab
-    customBuilder.intent.setPackage(packageName)
-    customBuilder.launchUrl(context, url)
+    // customBuilder.intent.setPackage(packageName)
+    // customBuilder.launchUrl(context, url)
+
+    val i = Intent(Intent.ACTION_VIEW, url)
+    activity?.startActivity(i)
 }
